5a5ba9a3f4b2642f54adb777a4f8148034768751
diogo <diogosf2006@gmail.com>
2025-07-21 11:22:36 -0300

    adicionado classes de  security

---
+++ src/main/java/com/agendador/infrastucture/entity/Telefone.java
@@ -0,0 +1,23 @@
+package com.agendador.infrastucture.entity;
+
+import  jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "telefone")
+
+public class Telefone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "numero" , length = 10)
+    private String numero;
+    @Column(name = "ddd" , length = 3)
+    private String ddd;
+}
